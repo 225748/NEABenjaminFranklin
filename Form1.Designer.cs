123a3bf90a2848201ee51f0687059758489ef2d0
Ben Franklin <225748@live.brock.ac.uk>
2024-10-11 20:23:12 +0100

    Login UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnUserLogin = new System.Windows.Forms.Button();
             this.btnHostLogin = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblEmail = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.picRotaConnect = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRotaConnect)).BeginInit();
             this.SuspendLayout();
             // 
             // txtEmail
@@ -81,39 +84,52 @@
             this.panel1.Controls.Add(this.btnHostLogin);
             this.panel1.Controls.Add(this.txtPassword);
             this.panel1.Controls.Add(this.btnUserLogin);
-            this.panel1.Location = new System.Drawing.Point(39, 27);
+            this.panel1.Location = new System.Drawing.Point(44, 162);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(248, 130);
             this.panel1.TabIndex = 5;
             // 
-            // lblEmail
-            // 
-            this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(18, 20);
-            this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(35, 13);
-            this.lblEmail.TabIndex = 5;
-            this.lblEmail.Text = "Email:";
-            // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(5, 61);
+            this.lblPassword.Location = new System.Drawing.Point(3, 61);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(56, 13);
             this.lblPassword.TabIndex = 6;
             this.lblPassword.Text = "Password:";
             // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(14, 20);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email:";
+            // 
+            // picRotaConnect
+            // 
+            this.picRotaConnect.Image = ((System.Drawing.Image)(resources.GetObject("picRotaConnect.Image")));
+            this.picRotaConnect.Location = new System.Drawing.Point(26, -10);
+            this.picRotaConnect.Name = "picRotaConnect";
+            this.picRotaConnect.Size = new System.Drawing.Size(291, 122);
+            this.picRotaConnect.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picRotaConnect.TabIndex = 6;
+            this.picRotaConnect.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 199);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(332, 316);
+            this.Controls.Add(this.picRotaConnect);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRotaConnect)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -127,6 +143,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.PictureBox picRotaConnect;
     }
 }
 
