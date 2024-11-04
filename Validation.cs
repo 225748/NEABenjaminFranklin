34d74e6f10223b748f1428248fef6ff985a777ce
Ben Franklin <225748@live.brock.ac.uk>
2024-11-04 12:19:27 +0000

    begin validation class

---
+++ Validation.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NEABenjaminFranklin
+{
+    public class Validation
+    {
+        public string plainTextValidation(string text)
+        {
+            const string regexExpression = @"^[a-zA-Z]$";
+            Match tryToMatch = Regex.Match(text, regexExpression);
+            if (tryToMatch.Success)
+            {
+                return "string";
+            }
+            else
+            {
+                return "invalid";
+            }
+            
+        }
+        public string emailValidation(string email)
+        {
+            const string regexExpression = @"  EXPRESSION GOES HERE $";
+            Match tryToMatch = Regex.Match(email, regexExpression);
+            if (tryToMatch.Success)
+            {
+                return "email";
+            }
+            else
+            {
+                return "invalid";
+            }
+        }
+        //SEE ONENote for interger stuff
+
+    }
+}
