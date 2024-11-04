62d1dec1a4a8291667067af3b94b99708c38e0cf
Ben Franklin <225748@live.brock.ac.uk>
2024-11-04 17:34:59 +0000

    plans and skeleton functions for page refresh in userlandingpage

--- frmUserLandingPage.cs
+++ frmUserLandingPage.cs
@@ -19,9 +19,25 @@ namespace NEABenjaminFranklin
 
         private void frmUserLandingPage_Load(object sender, EventArgs e)
         {
+            initaliseForm();
+        }
+
+        private void initaliseForm()
+        {
+            fillADDates();
+        }
+        private void picRefreshIcon_Click(object sender, EventArgs e)
+        {
+            //Clear all controls and lists etc then call
+            initaliseForm(); //to 'refresh' the page
+        }
+        private void fillADDates()
+        {
             //Filled list with testing values for now - fill lists from databases, be sure to include provision for no role
+
+            //In actual implementation, use a list of objects of a class and read it in a loop and then create a cc object for each
             List<string> userRotaDates = new List<string> { "12/10/2024", "19/10/2024", "30/03/2026", "12/09/2040" };
-            List<string> userRotaRoles = new List<string> { "Sound", "Door Team", "Visuals","NO ROLE" };
+            List<string> userRotaRoles = new List<string> { "Sound", "Door Team", "Visuals", "NO ROLE" };
             List<string> userRotaTimes = new List<string> { "10am", "10am", "4pm", "3pm" };
 
             for (int i = 0; i < userRotaDates.Count(); i++)
@@ -35,7 +51,7 @@ namespace NEABenjaminFranklin
                 ADModule.Show();
                 flpDates.Controls.Add(ADModule);
             }
-
         }
+
     }
 }
