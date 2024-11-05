0dd400dbb4aba8a033d389b878aaad00a33fa837
Ben Franklin <225748@live.brock.ac.uk>
2024-11-05 16:02:14 +0000

    created form for rota creation - rough plan in onenote - may need a new table linked to tblRota

---
+++ frmCreateNewRota.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace NEABenjaminFranklin
+{
+    partial class frmCreateNewRota
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCreateNewRota));
+            this.pnlTopBar = new System.Windows.Forms.Panel();
+            this.picRotaConnect = new System.Windows.Forms.PictureBox();
+            this.lblHostView = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlTopBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRotaConnect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTopBar.Controls.Add(this.picRotaConnect);
+            this.pnlTopBar.Controls.Add(this.lblHostView);
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.Size = new System.Drawing.Size(800, 31);
+            this.pnlTopBar.TabIndex = 15;
+            // 
+            // picRotaConnect
+            // 
+            this.picRotaConnect.Image = ((System.Drawing.Image)(resources.GetObject("picRotaConnect.Image")));
+            this.picRotaConnect.Location = new System.Drawing.Point(0, 0);
+            this.picRotaConnect.Name = "picRotaConnect";
+            this.picRotaConnect.Size = new System.Drawing.Size(121, 31);
+            this.picRotaConnect.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRotaConnect.TabIndex = 7;
+            this.picRotaConnect.TabStop = false;
+            // 
+            // lblHostView
+            // 
+            this.lblHostView.AutoSize = true;
+            this.lblHostView.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHostView.Location = new System.Drawing.Point(134, 3);
+            this.lblHostView.Name = "lblHostView";
+            this.lblHostView.Size = new System.Drawing.Size(93, 21);
+            this.lblHostView.TabIndex = 8;
+            this.lblHostView.Text = "Host Mode";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(254, 210);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "see one note for plan, may need to create a new table - see table design page";
+            // 
+            // frmCreateNewRota
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.pnlTopBar);
+            this.Name = "frmCreateNewRota";
+            this.Text = "frmCreateNewRota";
+            this.pnlTopBar.ResumeLayout(false);
+            this.pnlTopBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRotaConnect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTopBar;
+        private System.Windows.Forms.PictureBox picRotaConnect;
+        private System.Windows.Forms.Label lblHostView;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
