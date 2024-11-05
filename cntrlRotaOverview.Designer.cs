d18b75144884cf24afe28fea6311404394b280dd
Ben Franklin <225748@live.brock.ac.uk>
2024-11-05 20:44:20 +0000

    Custom Control for viewing all rotas

---
+++ cntrlRotaOverview.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace NEABenjaminFranklin
+{
+    partial class cntrlRotaOverview
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRotaName = new System.Windows.Forms.Label();
+            this.lblFacility = new System.Windows.Forms.Label();
+            this.btnEditRotaInfo = new System.Windows.Forms.Button();
+            this.btnMangeRotaInstances = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRotaName
+            // 
+            this.lblRotaName.AutoSize = true;
+            this.lblRotaName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRotaName.Location = new System.Drawing.Point(3, 10);
+            this.lblRotaName.Name = "lblRotaName";
+            this.lblRotaName.Size = new System.Drawing.Size(97, 16);
+            this.lblRotaName.TabIndex = 1;
+            this.lblRotaName.Text = "{Rota Name}";
+            // 
+            // lblFacility
+            // 
+            this.lblFacility.AutoSize = true;
+            this.lblFacility.Location = new System.Drawing.Point(3, 33);
+            this.lblFacility.Name = "lblFacility";
+            this.lblFacility.Size = new System.Drawing.Size(50, 13);
+            this.lblFacility.TabIndex = 2;
+            this.lblFacility.Text = "{locaton}";
+            // 
+            // btnEditRotaInfo
+            // 
+            this.btnEditRotaInfo.Location = new System.Drawing.Point(6, 54);
+            this.btnEditRotaInfo.Name = "btnEditRotaInfo";
+            this.btnEditRotaInfo.Size = new System.Drawing.Size(101, 23);
+            this.btnEditRotaInfo.TabIndex = 3;
+            this.btnEditRotaInfo.Text = "Edit Information";
+            this.btnEditRotaInfo.UseVisualStyleBackColor = true;
+            this.btnEditRotaInfo.Click += new System.EventHandler(this.btnEditRotaInfo_Click);
+            // 
+            // btnMangeRotaInstances
+            // 
+            this.btnMangeRotaInstances.Location = new System.Drawing.Point(6, 78);
+            this.btnMangeRotaInstances.Name = "btnMangeRotaInstances";
+            this.btnMangeRotaInstances.Size = new System.Drawing.Size(101, 23);
+            this.btnMangeRotaInstances.TabIndex = 4;
+            this.btnMangeRotaInstances.Text = "Edit Rota Dates";
+            this.btnMangeRotaInstances.UseVisualStyleBackColor = true;
+            this.btnMangeRotaInstances.Click += new System.EventHandler(this.btnMangeRotaInstances_Click);
+            // 
+            // cntrlRotaOverview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnMangeRotaInstances);
+            this.Controls.Add(this.btnEditRotaInfo);
+            this.Controls.Add(this.lblFacility);
+            this.Controls.Add(this.lblRotaName);
+            this.Name = "cntrlRotaOverview";
+            this.Size = new System.Drawing.Size(162, 106);
+            this.Load += new System.EventHandler(this.cntrlRotaOverview_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRotaName;
+        private System.Windows.Forms.Label lblFacility;
+        private System.Windows.Forms.Button btnEditRotaInfo;
+        private System.Windows.Forms.Button btnMangeRotaInstances;
+    }
+}
---
+++ cntrlRotaOverview.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NEABenjaminFranklin
+{
+    public partial class cntrlRotaOverview : UserControl
+    {
+        public string RotaName { get; set; }
+        public string Facility { get; set; }
+
+        public cntrlRotaOverview()
+        {
+            InitializeComponent();
+        }
+
+        private void cntrlRotaOverview_Load(object sender, EventArgs e)
+        {
+            lblRotaName.Text = RotaName;
+            lblFacility.Text = Facility;
+        }
+
+        private void btnEditRotaInfo_Click(object sender, EventArgs e)
+        {
+            //create an instance of the edit rota form and give it this info - use publics? - may need to put rota ID into this as a public
+
+        }
+
+        private void btnMangeRotaInstances_Click(object sender, EventArgs e)
+        {
+            //create an instance of the rota instances form and give it this info - use publics? - may need to put rota ID into this as a public
+
+        }
+
+    }
+}
