3f63c0f736f5c05fe27fa4ec880f8843b69fd2a1
Ben Franklin <225748@live.brock.ac.uk>
2024-11-12 11:27:26 +0000

    comment in clsUser

--- clsUser.cs
+++ clsUser.cs
@@ -3,7 +3,7 @@
 namespace NEABenjaminFranklin
 {
     public class clsUser
-    {
+    {//when a user logs in, assign their info to this class so their userID (and other bits if needed) can be used in sql statments
         public int userID { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
