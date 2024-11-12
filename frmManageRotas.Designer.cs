0d1f52ad75648531e5be1c70844218f2d8cbde99
Ben Franklin <225748@live.brock.ac.uk>
2024-11-12 12:37:07 +0000

    Minor resizing on managerotas form to work better with scrolling items

--- frmManageRotas.Designer.cs
+++ frmManageRotas.Designer.cs
@@ -45,7 +45,7 @@
             this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTopBar.Location = new System.Drawing.Point(0, 0);
             this.pnlTopBar.Name = "pnlTopBar";
-            this.pnlTopBar.Size = new System.Drawing.Size(677, 31);
+            this.pnlTopBar.Size = new System.Drawing.Size(698, 31);
             this.pnlTopBar.TabIndex = 15;
             // 
             // picRotaConnect
@@ -75,14 +75,14 @@
             this.flpRotas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.flpRotas.Location = new System.Drawing.Point(12, 38);
             this.flpRotas.Name = "flpRotas";
-            this.flpRotas.Size = new System.Drawing.Size(650, 315);
+            this.flpRotas.Size = new System.Drawing.Size(674, 315);
             this.flpRotas.TabIndex = 16;
             // 
             // frmManageRotas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(677, 365);
+            this.ClientSize = new System.Drawing.Size(698, 365);
             this.Controls.Add(this.flpRotas);
             this.Controls.Add(this.pnlTopBar);
             this.Name = "frmManageRotas";
