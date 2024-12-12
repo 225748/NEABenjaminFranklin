e12d55aa26536cfb2b310ea60764ab4086566913
Ben Franklin <225748@live.brock.ac.uk>
2024-12-12 16:07:06 +0000

    Code for checking for an exisitng assignedrotarole done - NOT TESTED YET

--- cntrlRoleWithListVUsers.cs
+++ cntrlRoleWithListVUsers.cs
@@ -15,6 +15,8 @@ namespace NEABenjaminFranklin
     {
         public string RoleName { get; set; }
         public int RotaRoleNumber { get; set; }
+        public int RotaID { get; set; }
+
         public List<int> AssignedRotaRoleIDs = new List<int>();
         public cntrlRoleWithListVUsers()
         {
@@ -57,19 +59,51 @@ namespace NEABenjaminFranklin
             dbConnector.Close();
             return 0;
         }
+
+        private int CheckForExistingAssignment(ListViewItem CheckedItem)
+        {
+            int userID = Convert.ToInt32(CheckedItem.SubItems[1].Text);
+            clsDBConnector dbConnector = new clsDBConnector();
+            OleDbDataReader dr;
+            string sqlCommand = "SELECT tblAssignedRotaRoles.AssignedRotaRolesID " +
+                "FROM((tblAssignedRotaRoles INNER JOIN " +
+                "tblRotaRoles ON tblAssignedRotaRoles.RotaRoleNumber = tblRotaRoles.RotaRoleNumber AND tblAssignedRotaRoles.RotaRoleNumber = tblRotaRoles.RotaRoleNumber AND " +
+                "tblAssignedRotaRoles.RotaRoleNumber = tblRotaRoles.RotaRoleNumber) INNER JOIN " +
+                "tblRota ON tblRotaRoles.RotaID = tblRota.RotaID) " +
+                $"WHERE (tblAssignedRotaRoles.UserID = {userID}) " +
+                $"AND (tblRotaRoles.RotaID = {RotaID}) " +
+                $"AND (tblRotaRoles.RoleNumber = {RotaRoleNumber})";
+            dbConnector.Connect();
+            dr = dbConnector.DoSQL(sqlCommand);
+            while (dr.Read())
+            {
+                dbConnector.Close();
+                return Convert.ToInt32(dr[0].ToString()); //return AssignedRotaRoleID if there is one
+            }
+            dbConnector.Close();
+            return 0; //if there isn't one
+        }
         public void AssignUsersToRotaRole()
         {
             for (int i = 0; i < lstVUsers.CheckedItems.Count; i++)
             {
                 //First check if they already have been assigned this rota role, if so do nothing
-                //else
-                clsDBConnector dbConnector = new clsDBConnector();
-                string cmdStr = $"INSERT INTO tblAssignedRotaRoles (RotaRoleNumber, UserID) " +
-                    $"VALUES ({RotaRoleNumber}, '{Convert.ToInt32(lstVUsers.CheckedItems[i].SubItems[1].Text)}')";
-                dbConnector.Connect();
-                dbConnector.DoDML(cmdStr);
-                dbConnector.Close();
-                AssignedRotaRoleIDs.Add(FindLargestID("tblAssignedRotaRoles", "AssignedRotaRolesID"));
+                int existingAssignmentID = CheckForExistingAssignment(lstVUsers.CheckedItems[i]);
+                if (existingAssignmentID == 0) //none already existing, therfore can add a new one
+                {
+                    clsDBConnector dbConnector = new clsDBConnector();
+                    string cmdStr = $"INSERT INTO tblAssignedRotaRoles (RotaRoleNumber, UserID) " +
+                        $"VALUES ({RotaRoleNumber}, '{Convert.ToInt32(lstVUsers.CheckedItems[i].SubItems[1].Text)}')";
+                    dbConnector.Connect();
+                    dbConnector.DoDML(cmdStr);
+                    dbConnector.Close();
+                    AssignedRotaRoleIDs.Add(FindLargestID("tblAssignedRotaRoles", "AssignedRotaRolesID"));
+                }
+                else //one already existing, add to assignedrotarolesIds list the assignedrotaRoleID returned from the check
+                {
+                    AssignedRotaRoleIDs.Add(existingAssignmentID);
+                }
+
             }
         }
         
