ffd70f4c827f7bd01fac9d082c0af7771670bb55
Ben Franklin <225748@live.brock.ac.uk>
2024-12-16 11:26:47 +0000

    Trying to sort out github commits

---
+++ frmEditAddInstance.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace NEABenjaminFranklin
+{
+    partial class frmAddNewInstance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddNewInstance));
+            this.pnlTopBar = new System.Windows.Forms.Panel();
+            this.picRotaConnect = new System.Windows.Forms.PictureBox();
+            this.lblHostView = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblRotaName = new System.Windows.Forms.Label();
+            this.btnThemeColour = new System.Windows.Forms.Button();
+            this.lblFacility = new System.Windows.Forms.Label();
+            this.flpRoles = new System.Windows.Forms.FlowLayoutPanel();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.btnAddInstance = new System.Windows.Forms.Button();
+            this.pnlTopBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRotaConnect)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTopBar.Controls.Add(this.picRotaConnect);
+            this.pnlTopBar.Controls.Add(this.lblHostView);
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.Size = new System.Drawing.Size(356, 31);
+            this.pnlTopBar.TabIndex = 17;
+            // 
+            // picRotaConnect
+            // 
+            this.picRotaConnect.Image = ((System.Drawing.Image)(resources.GetObject("picRotaConnect.Image")));
+            this.picRotaConnect.Location = new System.Drawing.Point(0, 0);
+            this.picRotaConnect.Name = "picRotaConnect";
+            this.picRotaConnect.Size = new System.Drawing.Size(121, 31);
+            this.picRotaConnect.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRotaConnect.TabIndex = 7;
+            this.picRotaConnect.TabStop = false;
+            // 
+            // lblHostView
+            // 
+            this.lblHostView.AutoSize = true;
+            this.lblHostView.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHostView.Location = new System.Drawing.Point(134, 3);
+            this.lblHostView.Name = "lblHostView";
+            this.lblHostView.Size = new System.Drawing.Size(93, 21);
+            this.lblHostView.TabIndex = 8;
+            this.lblHostView.Text = "Host Mode";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblRotaName);
+            this.panel1.Controls.Add(this.btnThemeColour);
+            this.panel1.Controls.Add(this.lblFacility);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(356, 29);
+            this.panel1.TabIndex = 21;
+            // 
+            // lblRotaName
+            // 
+            this.lblRotaName.AutoSize = true;
+            this.lblRotaName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRotaName.Location = new System.Drawing.Point(16, 6);
+            this.lblRotaName.Name = "lblRotaName";
+            this.lblRotaName.Size = new System.Drawing.Size(97, 16);
+            this.lblRotaName.TabIndex = 17;
+            this.lblRotaName.Text = "{Rota Name}";
+            // 
+            // btnThemeColour
+            // 
+            this.btnThemeColour.BackColor = System.Drawing.Color.Silver;
+            this.btnThemeColour.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnThemeColour.Enabled = false;
+            this.btnThemeColour.FlatAppearance.BorderSize = 0;
+            this.btnThemeColour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThemeColour.Location = new System.Drawing.Point(0, 0);
+            this.btnThemeColour.Name = "btnThemeColour";
+            this.btnThemeColour.Size = new System.Drawing.Size(10, 29);
+            this.btnThemeColour.TabIndex = 19;
+            this.btnThemeColour.UseVisualStyleBackColor = false;
+            // 
+            // lblFacility
+            // 
+            this.lblFacility.AutoSize = true;
+            this.lblFacility.Location = new System.Drawing.Point(682, 8);
+            this.lblFacility.Name = "lblFacility";
+            this.lblFacility.Size = new System.Drawing.Size(50, 13);
+            this.lblFacility.TabIndex = 18;
+            this.lblFacility.Text = "{locaton}";
+            // 
+            // flpRoles
+            // 
+            this.flpRoles.AutoScroll = true;
+            this.flpRoles.Location = new System.Drawing.Point(19, 108);
+            this.flpRoles.Name = "flpRoles";
+            this.flpRoles.Size = new System.Drawing.Size(314, 291);
+            this.flpRoles.TabIndex = 22;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(73, 82);
+            this.dtpDate.MinDate = new System.DateTime(2024, 10, 29, 0, 0, 0, 0);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(93, 20);
+            this.dtpDate.TabIndex = 23;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(37, 84);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 24;
+            this.lblDate.Text = "Date";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(182, 84);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 26;
+            this.lblTime.Text = "Time";
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTime.Location = new System.Drawing.Point(218, 82);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.ShowUpDown = true;
+            this.dtpTime.Size = new System.Drawing.Size(93, 20);
+            this.dtpTime.TabIndex = 25;
+            // 
+            // btnAddInstance
+            // 
+            this.btnAddInstance.Location = new System.Drawing.Point(202, 405);
+            this.btnAddInstance.Name = "btnAddInstance";
+            this.btnAddInstance.Size = new System.Drawing.Size(109, 23);
+            this.btnAddInstance.TabIndex = 27;
+            this.btnAddInstance.Text = "Add a new Date";
+            this.btnAddInstance.UseVisualStyleBackColor = true;
+            this.btnAddInstance.Click += new System.EventHandler(this.btnAddInstance_Click);
+            // 
+            // frmAddNewInstance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 450);
+            this.Controls.Add(this.btnAddInstance);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.flpRoles);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlTopBar);
+            this.Name = "frmAddNewInstance";
+            this.Text = "frmAddNewInstance";
+            this.Load += new System.EventHandler(this.frmAddNewInstance_Load);
+            this.pnlTopBar.ResumeLayout(false);
+            this.pnlTopBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRotaConnect)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTopBar;
+        private System.Windows.Forms.PictureBox picRotaConnect;
+        private System.Windows.Forms.Label lblHostView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblRotaName;
+        private System.Windows.Forms.Button btnThemeColour;
+        private System.Windows.Forms.Label lblFacility;
+        private System.Windows.Forms.FlowLayoutPanel flpRoles;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.Button btnAddInstance;
+    }
+}
