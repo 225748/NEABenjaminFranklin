cb81dca6afe3f8c6c2362d9fe59e7f3d60d990c4
Ben Franklin <225748@live.brock.ac.uk>
2025-02-11 14:27:09 +0000

    Hasher plan and salt generator started

---
+++ clsPasswordHasher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEABenjaminFranklin
+{
+    internal class clsPasswordHasher
+    {
+        //create a random salt unique to each user and store in people table
+        //append the salt to their raw password
+        //conv to binary
+        //ensure at a given size of bits - append 1 and then 0s to get to the size
+        //perform bitwise operations and pads, shifts rotates etc
+        //conv to hex and store in database
+        private string GenerateSalt()
+        {
+            string salt = "";
+            Random randValue = new Random();
+            for (int i = 0; i < 256; i++)
+            {
+                int ascii = randValue.Next(65, 90);
+                if (ascii % 2 == 1)
+                {//add the lowercase version for variety
+                    ascii = ascii + 32;
+                }
+                salt = salt + (Convert.ToChar(ascii));
+            }
+            return salt;
+
+            
+        }
+
+    }
+}
