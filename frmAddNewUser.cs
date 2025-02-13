57f468ae33dc04436edc1320999c950d275bd9e5
Ben Franklin <225748@live.brock.ac.uk>
2025-02-13 20:10:45 +0000

    add new user email working

--- frmAddNewUser.cs
+++ frmAddNewUser.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography;
@@ -19,10 +20,77 @@ namespace NEABenjaminFranklin
         {
             InitializeComponent();
         }
+        private int FindLargestID(string tableName, string keyName)//largest ID is allways the one you have just created
+        {
+            clsDBConnector dbConnector = new clsDBConnector();
+            OleDbDataReader dr;
+            string sqlStr;
+            dbConnector.Connect();
+            sqlStr = $" SELECT MAX({keyName}) AS maxID FROM {tableName} ";
+            dr = dbConnector.DoSQL(sqlStr);
+            int ret = 0;
+            while (dr.Read())
+            {
+                ret = Convert.ToInt32(dr[0]);
+            }
+            dbConnector.Close();
+            return ret;
+        }
+
+        private bool Email(string email, string tempPassword, int userID)
+        {
+            //Get first and last name
+            string firstName = ""; string lastName = "";
+            clsDBConnector dbConnector = new clsDBConnector();
+            OleDbDataReader dr;
+            string sqlCommand = "SELECT FirstName, LastName " +
+                "FROM tblPeople " +
+                $"WHERE UserID = {userID}";
+            dbConnector.Connect();
+            dr = dbConnector.DoSQL(sqlCommand);
+
+            while (dr.Read())
+            {
+                firstName = dr[0].ToString();
+                lastName = dr[1].ToString();
+            }
+            dbConnector.Close();
+
+
+            //Email
+            clsEmailManager emailManager = new clsEmailManager();
+
+            //Retrieve template
+            string templateFilePath = (AppDomain.CurrentDomain.BaseDirectory + "/Html_Email_Templates/newUserEmailTemplate.html");//directs to its own debug folder and then the file
+
+            //Make an array of variables to replace in the html template
+            clshtmlVariable[] variableReplacements = new clshtmlVariable[4];//num of unique variables in html template
+
+            //for every unique variable in the template do this
+            variableReplacements[0] = new clshtmlVariable("{firstName}", firstName);
+            variableReplacements[1] = new clshtmlVariable("{lastName}", lastName);
+            variableReplacements[2] = new clshtmlVariable("{email}", email);
+            variableReplacements[3] = new clshtmlVariable("{temporaryPassword}", tempPassword);
+
+
+            bool successfull = false;
+
+            string htmlBody = emailManager.ReadAndPopulateEmailTemplate(templateFilePath, variableReplacements);
+            try
+            {
+                emailManager.SendEmail(email, htmlBody, "Your new RotaConnect Account!");
+                successfull = true;
+            }
+            catch (Exception)
+            {
+                successfull = false; //will need to manually communicate the temp password
+            }
+            return successfull;
+        }
 
         private void btnAddUser_Click(object sender, EventArgs e)
         {
-            //create validate email function
+            //validate email
             Validation validator = new Validation();
             string response = validator.emailValidation(txtEmail.Text);
             string validatedEmail = "";
@@ -32,7 +100,7 @@ namespace NEABenjaminFranklin
             }
             else
             {
-                MessageBox.Show("Please enter a valid email","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Please enter a valid email", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return; //breaks out the function
             }
 
@@ -55,27 +123,11 @@ namespace NEABenjaminFranklin
             //hash password using a function
             clsPasswordHasher passwordHasher = new clsPasswordHasher();
             string salt = passwordHasher.GenerateSalt();
-            string hashedPassword = passwordHasher.PerformHash(tempPassword,salt);
-
-            /////////////////////////////
-            //Need too code
-            //Eventually email this temporary password (using a seperate function) to the new user
-            //CREATE AN EMAIL CLASS TO CREATE OBJECTS OFF WHEN SENDING EMAIL
-            ////////////////////////////
-            ///
-            bool successfull = Email();
+            string hashedPassword = passwordHasher.PerformHash(tempPassword, salt);
 
 
-            //if email not sucessfully sent
-            MessageBox.Show("There was an issue emailing the temporary password to the new user" +
-                "\n\nThis information will not be viewable once this window is closed" +
-                "\n\nPlease communicate the following manually:" +
-                $"\n\n Email: {validatedEmail}" +
-                $"\n One-Time Temporary Password: {tempPassword}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            Clipboard.SetText(tempPassword);
-
+            //insert new user into people table
             bool successfulUserCreation = false;
-            //insert data into people table
             try
             {
                 clsDBConnector dbConnector = new clsDBConnector();
@@ -94,16 +146,36 @@ namespace NEABenjaminFranklin
             }
             if (!successfulUserCreation)
             {
-                //error message
-            }
-            else
-            {
-                MessageBox.Show("User successfully created", "Rota Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                //send email to new user containing login details (email signed up and temporay password)
-                //try except that, if error, display messaage box containing temp password to communicate to user manually
-                //if all goes well display a message box saying user created and login email sent
+                MessageBox.Show("Error adding user to database\nUser has not been created", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 this.Close();
             }
+            MessageBox.Show("User successfully created", "Rota Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+
+            //get the new userID
+            int userID = FindLargestID("tblPeople", "UserID");
+
+            //Communicate temp password to new user via email
+            bool successfull = Email(validatedEmail, tempPassword, userID);
+
+            //if email not sucessfully sent
+            if (!successfull)
+            {
+                MessageBox.Show("There was an issue emailing the temporary password to the new user" +
+                    "\n\nThis information will not be viewable once this window is closed" +
+                    "\n\nPlease communicate the following manually:" +
+                    $"\n\n Email: {validatedEmail}" +
+                    $"\n One-Time Temporary Password: {tempPassword}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Clipboard.SetText(tempPassword);
+            }
+            if (successfull)
+            {
+                MessageBox.Show("Email with temporary password successfully sent", "Rota Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            this.Close();
+
         }
 
     }
